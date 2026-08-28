7db1ed3ca65d232469a43892085ef012b42187b0
Axe.Xu <axeprpr@163.com>
2026-08-28 17:53:08 +0000

    移除部署地址并动态生成技能文档

--- skills.go
+++ skills.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"net/http"
+	"strings"
 )
 
 //go:embed skills.md
@@ -15,5 +16,13 @@ func skills(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "text/markdown; charset=utf-8")
 	w.Header().Set("Cache-Control", "no-cache")
-	_, _ = w.Write(skillInstructions)
+	protocol := "http"
+	if r.TLS != nil {
+		protocol = "https"
+	} else if forwarded := r.Header.Get("X-Forwarded-Proto"); forwarded != "" {
+		protocol = strings.TrimSpace(strings.Split(forwarded, ",")[0])
+	}
+	baseURL := protocol + "://" + r.Host
+	content := strings.ReplaceAll(string(skillInstructions), "{{BASE_URL}}", baseURL)
+	_, _ = w.Write([]byte(content))
 }
